85d6fe8373577cff9726431355393d1abe6e3f9a
suaebahmed <suaebahmed12@gmail.com>
2020-02-14 13:16:15 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Bookform from './components/Bookform';
 function App() {
   return (
     <div className="App">
+        <h1>This is my second commit</h1>
+        <p>I am learning how I can it</p>
         <BookContextP>
             <Navbar></Navbar>
             <Bookform></Bookform>
